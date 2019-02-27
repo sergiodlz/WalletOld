fd911f44ca02b00a53b3b653f288a26d6962bd92
Sergio De La Hoz <sdelahozcarrillo@gmail.com>
2019-02-27 13:10:32 -0500

    Initial Commit

---
+++ Wallet.API/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace Wallet.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            var values = new string[] { "value1", "value2" };
+            _logger.LogInformation("{values}", values);
+            return values;
+        }
+    }
+}
---
+++ Wallet.API/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Serilog;
+using Serilog.Events;
+
+namespace Wallet.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .WriteTo.File("WarningLog_.txt", rollingInterval: RollingInterval.Day, 
+                    retainedFileCountLimit: 62, rollOnFileSizeLimit: true, 
+                    fileSizeLimitBytes: 2000000, shared: true)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting web host");
+                CreateWebHostBuilder(args).Build().Run();
+            }
+            catch (System.Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+            
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseSerilog();
+    }
+}
