2e2327325cd6688a04a62e029b60eeb40ff3cdc7
Sergio De La Hoz <sdelahozcarrillo@gmail.com>
2019-03-06 09:26:46 -0500

    Servicios de configuración de Startup

--- Wallet.API/Startup.cs
+++ Wallet.API/Startup.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Wallet.API.Extensions;
 
 namespace Wallet.API
 {
@@ -17,6 +19,10 @@ namespace Wallet.API
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.ConfigureCors();
+
+            services.ConfigureIISIntegration();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -26,8 +32,21 @@ namespace Wallet.API
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
             app.UseHttpsRedirection();
+
+            app.UseCors("CorsPolicy");
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.All
+            });
+
             app.UseMvc();
         }
     }
